3c0926e5a6b2b5f08eff001aa3a00a92a86b14ad
Naza Delucca <nazadelucca@gmail.com>
2019-05-21 16:19:00 -0300

    Show single post & create post functionality added

---
+++ api/post/read_single.php
@@ -0,0 +1,32 @@
+<?php
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Post.php';
+
+    // Instantiate DB & Connect
+    $database = new Database();
+    $db = $database->connect();
+
+    // Instantiate blog post object
+    $post = new Post($db);
+
+    // Get ID
+    $post->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+    //Get Blog Post
+    $post->read_single();
+
+    // Create array for json data
+    $post_arr = array(
+        'id' => $post->id,
+        'title' => $post->title,
+        'body' => html_entity_decode($post->body),
+        'author' => $post->author,
+        'category_id' => $post->category_id,
+        'category_name' => $post->category_name
+    );
+
+    echo json_encode($post_arr);
